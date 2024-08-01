56a9383404976f1b57d8a67a7dcd803d0adb9f87
marian <puubles@gmail.com>
2024-08-02 00:12:52 +0300

    test

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express, { Application, Request, Response, NextFunction } from "express";
+import * as dotenv from "dotenv";
+dotenv.config();
 const api = express();
 const port = process.env.PORT || 8080;
 import cors, { CorsOptions } from "cors";
@@ -28,18 +30,21 @@ import apiRouter from "./routes/apiRoutes";
 import mongoose from "mongoose";
 import { login, authWithToken } from "./controllers/AuthController";
 
-const MONGO_URI =
-  process.env.MONGO_URI ||
-  "mongodb://mongo:7f7c8b0ec3a9264da6de@78.135.107.15:2333/puuv_v1?authSource=admin";
+const MONGO_URI = process.env.MONGO_URI;
+if (MONGO_URI) {
+  try {
+    mongoose
+      .connect(MONGO_URI, {})
+      .then(() => {})
+      .catch((error) => {
+        console.error("Error connecting to MongoDB", error);
+      });
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log(error, "try catch");
+  }
+}
 
-mongoose
-  .connect(MONGO_URI, {})
-  .then(() => {})
-  .catch((error) => {
-    console.error("Error connecting to MongoDB", error);
-  });
-
-console.log("Connected to MongoDB");
 api.use(express.json());
 api.use("/api/", authenticateWithToken, apiRouter);
 api.use("/user/", authenticateJWT, apiRouter);
