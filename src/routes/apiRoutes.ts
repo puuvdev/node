36b27407f2aeca624d946c7b35a761000b767b82
marian <puubles@gmail.com>
2024-10-18 14:50:35 +0300

    api routes

--- src/routes/apiRoutes.ts
+++ src/routes/apiRoutes.ts
@@ -1,10 +1,11 @@
 import { Router, Request, Response } from "express";
 import { main } from "../controllers/MainController";
-import { login } from "../controllers/AuthController";
+import { login, register } from "../controllers/AuthController";
 
 const router = Router();
 
 router.post("/process", main);
 router.post("/login", login);
+router.post("/register", register);
 
 export default router;
